4a864dfdcf7d5a7d86d48c6fbe212c80e4a8e56e
Racana <pablo.racana@gmail.com>
2020-12-30 22:35:36 -0300

    requirements.txt

--- Ole/ole-scrapper.py
+++ Ole/ole-scrapper.py
@@ -6,6 +6,8 @@ from tenacity import *
 from datetime import datetime
 import pandas as pd
 import locale
+import numpy as np
+import random
 
 parser = argparse.ArgumentParser(description='Webscraper para el diario deportivo Ole, obtiendo datos de la superliga')
 parser.add_argument('--jornada', 
@@ -23,6 +25,11 @@ source = r'C:\Users\El_Ra\Google Drive\GranDT\grandt-scrapper\Driver\chromedrive
 page = 'https://www.ole.com.ar/estadisticas/futbol/primera-division.html' ## podemos cambiar la url para obtener datos de otras temporadas
 
 datos_partido = []
+columns_datos_partido = ['jornada', 'match_id', 'equipo local', 'equipo_visitante', 
+                        'ole_id_local', 'ole_id_visita', 'arbitro', 'estadio', 'fecha', 
+                        'goles_local', 'goles_visita', 'resultado']
+
+data_eventos = pd.DataFrame()
 
 @retry(wait=wait_fixed(3), stop=stop_after_attempt(10)) #implementamos retry en caso de que haya publicidad en pantalla completa
 def retry_click(element):
@@ -62,13 +69,17 @@ def match_list_func(driver):
     match_list = tabs_selected.find_elements_by_xpath('.//a[@class="opta-event-link external-link"][contains(text(), "Detalles")]')
     return [mid.get_attribute("href") for mid in match_list]
 
-def datos_partido_func(driver):
+def datos_partido_func(driver, match):
+    global ole_id_local, ole_id_visita, equipo_local, equipo_visitante, match_id
+
     local_elem = driver.find_element_by_xpath(".//span[contains(@class, 'home-team team team')]")
     vist_elem = driver.find_element_by_xpath(".//span[contains(@class, 'away-team team team')]")
     equipo_local = local_elem.text
     equipo_visitante = vist_elem.text
     ole_id_local = local_elem.get_attribute('class').split("-")[-1]
     ole_id_visita = vist_elem.get_attribute('class').split("-")[-1]
+
+    match_id = match.split('=')[-1]
 
     detalles_partido = driver.find_element_by_xpath("//div[@class='match-details']/dl")
     detalles_partido_lista = detalles_partido.find_elements_by_xpath(".//dd")
@@ -85,9 +96,34 @@ def datos_partido_func(driver):
     goles_visita = int(resultado_partido.split(" - ")[1])
     resultado = 'local' if goles_local > goles_visita else 'visitante' if goles_visita > goles_local else 'empate'
 
-    return (equipo_local, equipo_visitante, ole_id_local, ole_id_visita, arbitro, estadio, fecha, goles_local, goles_visita, resultado)
+    return (jornada, match_id, equipo_local, equipo_visitante, ole_id_local, ole_id_visita, arbitro, estadio, fecha, goles_local, goles_visita, resultado)
 
-def run():
+def eventos_partido_func(driver, data_eventos):
+    eventos_local = driver.find_elements_by_xpath("//select[@class='event-selection home']/option")
+    eventos_visita = driver.find_elements_by_xpath("//select[@class='event-selection away']/option")
+    total_eventos = eventos_local[1:] + eventos_visita[1:]
+    for evento in total_eventos:
+        time.sleep(random())
+
+        retry_click(evento)
+        if evento in eventos_local:
+            id_equipo = ole_id_local
+        else: id_equipo = ole_id_visita
+            
+        df_eventos_partido = pd.read_html(driver.page_source)[5]
+        df_eventos_partido['match_id'] = match_id
+        df_eventos_partido['id_equipo'] = np.where(df_eventos_partido[3] == equipo_local, ole_id_local, ole_id_visita)
+        df_eventos_partido.rename(columns={0:'minuto',
+                        1:'numero',
+                        2:'jugador',
+                        3:'equipo',
+                        4:'evento'}, 
+                inplace=True)
+        data_eventos = data_eventos.append(df_eventos_partido)
+    
+    return data_eventos
+
+def run(data_eventos):
     driver = webdriver.Chrome(source)
     driver.maximize_window()
 
@@ -98,12 +134,15 @@ def run():
 
     for match in match_list:
         driver.get(match)
-        datos_partido.append(datos_partido_func(driver))
-    columns_datos_partido = ['equipo local', 'equipo_visitante', 'ole_id_local', 'ole_id_visita', 'arbitro', 'estadio', 'fecha', 'goles_local', 'goles_visita', 'resultado']
+        datos_partido.append(datos_partido_func(driver, match))
+        data_eventos = eventos_partido_func(driver, data_eventos)
+    
     df_datos_partido = pd.DataFrame(data=datos_partido, columns=columns_datos_partido)
-    df_datos_partido.to_csv('datos_partido.csv')
+    df_datos_partido.to_csv('datos_partido.csv', index=False)
+
+    data_eventos.to_csv('data_eventos.csv', index=False)
 
 if __name__ == '__main__':
-    run()
+    run(data_eventos)
 
 
